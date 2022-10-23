401bb3598e1eb3adf6078593538bf014595c263a
Wanessa Koserski <wkoserski@gmail.com>
2022-10-22 23:39:06 -0300

    :recycle: refactor: capture the past number and print sprite accordingly on the map

--- src/map.h
+++ src/map.h
@@ -6,6 +6,7 @@ typedef struct SPRITE_MAP
     ALLEGRO_BITMAP* _sheet;
 
     ALLEGRO_BITMAP* checkered_floor;
+    ALLEGRO_BITMAP* left_corner_red_wall;
 }SPRITE_MAP;
 SPRITE_MAP sprite_map;
 
@@ -38,77 +39,24 @@ void init_sprites_map(void)
     return;
 }
 
-/*
-int** load_map(const char* filename, int* lines, int* columns)
-{
-    FILE* file = fopen(filename, "r");
-    int** matriz;
-
-    if(file != NULL)
-    {
-        fscanf(file, "%d %d", lines, columns);
-
-        matriz = (int**) malloc( (*lines) * sizeof(int*) );
-
-        for(int i = 0; i < *lines; i++)
-        {
-            for(int j = 0; j < *columns; j++)
-            {
-                fscanf(file, "%d", &matriz[i][j]);
-            }
-        }
-
-        fclose(file);
-    }
-
-    return matriz;
-}
-
-
-void map_draw(ALLEGRO_BITMAP* buffer, int** map, int lines, int columns)
-{
-    for(int i = 0; i < lines; i++)
-    {
-        for(int j = 0; j < columns; j++)
-        {
-            if(map[i][j] == FLOOR)
-            al_draw_bitmap(buffer, j * tile_size, i * tile_size, 0);
-        }
-    }
-
-    return;
-}
-
-
-void destroy_map(int** map, int lines)
-{
-    for(int i = 0; i < lines; i++)
-        free(map[i]);
-    
-    free(map);
-
-    return;
-}
-*/
-
 int xOff = 0;
 int yOff = 0;
-int map_columns = 10;
-int map_size = 120;
+const int map_columns = 10;
+const int map_lines = 10;
 int tile_size = 48;
 
-int map[] = {
-                1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-                1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 
-                1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 
-                1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 
-                1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 
-                1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 
-                1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 
-                1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 
-                1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 
-                1, 1, 1, 1, 1, 1, 1, 1, 1, 1
-            };
+int map[10][10] = { 
+                    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                    {2, 2, 2, 2, 2, 2, 2, 2, 2, 2},
+                    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                    {2, 2, 2, 2, 2, 2, 2, 2, 2, 2},
+                    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                    {2, 2, 2, 2, 2, 2, 2, 2, 2, 2},
+                    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                    {2, 2, 2, 2, 2, 2, 2, 2, 2, 2},
+                    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+                   };
 
 void map_update(void)
 {
@@ -126,9 +74,15 @@ void map_update(void)
 
 void map_draw()
 {
-    for(int i = 0; i < map_size; i++)
-    {
-        al_draw_bitmap(sprite_map.checkered_floor, xOff + tile_size * (i % map_columns), yOff + tile_size * (i / map_columns), 0);
+    int count = 0;
+    for(int i = 0; i < 10; i++)
+    {   
+        for(int j = 0; j < 10; j++) 
+        {
+            if(map[i][j] == 1)
+            al_draw_bitmap(sprite_map.checkered_floor, xOff + tile_size * (count % 10), yOff + tile_size * (count / 10), 0);   
+            count++;
+        }    
     }
     return;
 }
