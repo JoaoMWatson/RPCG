bf2eb1a55d1673e2d111acf9ccd4e4d0ac6b7266
Wanessa Koserski <wkoserski@gmail.com>
2022-10-27 02:05:56 -0300

    :test_tube: test: using new sprite and structure tiles with txt

--- src/map.h
+++ src/map.h
@@ -6,19 +6,34 @@ typedef struct SPRITE_MAP
     ALLEGRO_BITMAP* _sheet;
 
     ALLEGRO_BITMAP* checkered_floor;
-    ALLEGRO_BITMAP* left_corner_red_wall;
+    ALLEGRO_BITMAP* voiding;
+    ALLEGRO_BITMAP* corner_top_left;
+    ALLEGRO_BITMAP* corner_top_right;
+    ALLEGRO_BITMAP* corner_bottom_left;
+    ALLEGRO_BITMAP* corner_bottom_right;
+    ALLEGRO_BITMAP* corner_left;
+    ALLEGRO_BITMAP* corner_right;
+    ALLEGRO_BITMAP* corner_top;
+    ALLEGRO_BITMAP* corner_bottom;
+    ALLEGRO_BITMAP* floor;
+    ALLEGRO_BITMAP* wall;
 }SPRITE_MAP;
 SPRITE_MAP sprite_map;
 
-/*
-const int tile_size = 50;
-
-enum
+enum sprite_block
 {
-    FLOOR = 0
+    VOIDING = 10,
+    CORNER_TOP_LEFT = 20,
+    CORNER_TOP_RIGHT = 30,
+    CORNER_BOTTOM_LEFT = 40,
+    CORNER_BOTTOM_RIGHT = 50,
+    CORNER_LEFT = 60,
+    CORNER_RIGHT = 70,
+    CORNER_TOP = 80,
+    CORNER_BOTTOM = 90,
+    FLOOR = 100,
+    WALL = 110
 };
-*/
-
 
 ALLEGRO_BITMAP* grab_sprite_map(int x, int y, int w, int h)
 {
@@ -33,30 +48,35 @@ void init_sprites_map(void)
     sprite_map._sheet = al_load_bitmap("src/images/inside.png");
     must_init(true, sprite_map._sheet, "inside map spritesheet");
     
-    if(sprite_map._sheet != NULL)
-        sprite_map.checkered_floor = grab_sprite_map(36, 0, 36, 36);
+    if(sprite_map._sheet != NULL) {
+        sprite_map.checkered_floor = grab_sprite_map(64, 0, 32, 32);
+        sprite_map.voiding = grab_sprite_map(32, 32, 32, 32);
+        sprite_map.corner_bottom_right = grab_sprite_map(96, 64, 32, 32);
+        sprite_map.corner_bottom = grab_sprite_map(64, 32, 32, 32);
+        sprite_map.corner_right = grab_sprite_map(96, 32, 32, 32);
+        sprite_map.wall= grab_sprite_map(0, 32, 32, 32);
+    }
 
     return;
 }
 
 int xOff = 0;
 int yOff = 0;
-const int map_columns = 10;
+const int map_columns = 43;
 const int map_lines = 10;
-int tile_size = 36;
+int tile_size = 32;
 
-int map[10][10] = { 
-                    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-                    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-                    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-                    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-                    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-                    {2, 2, 2, 2, 2, 2, 2, 2, 2, 2},
-                    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-                    {2, 2, 2, 2, 2, 2, 2, 2, 2, 2},
-                    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-                    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
-                   };
+
+int map[43][10] =   { 
+                                        {10, 10, 10, 10, 10, 10, 10, 10, 10, 10},
+                                        {10, 50, 90, 90, 90, 90, 90, 90, 90, 90},
+                                        {10, 70, 110, 110, 110, 110, 110, 110, 110, 110},
+                                        {10, 70, 100, 100, 100, 100, 100, 100, 100, 100},
+                                        {10, 70, 100, 100, 100, 100, 100, 100, 100, 100},
+                                        {10, 70, 100, 100, 100, 100, 100, 100, 100, 100},
+                                        {10, 70, 100, 100, 100, 100, 100, 100, 100, 100}
+                                    };
+
 
 void map_update(void)
 {
@@ -75,12 +95,44 @@ void map_update(void)
 void map_draw()
 {
     int count = 0;
-    for(int i = 0; i < 10; i++)
+    for(int i = 0; i < map_lines; i++)
     {   
-        for(int j = 0; j < 10; j++) 
+        for(int j = 0; j < map_columns; j++) 
         {
-            if(map[i][j] == 1)
-            al_draw_bitmap(sprite_map.checkered_floor, xOff + tile_size * (count % 10), yOff + tile_size * (count / 10), 0);   
+            if(map[i][j] == VOIDING) {
+                al_draw_bitmap(sprite_map.voiding, xOff + tile_size * (count % 10), yOff + tile_size * (count / 10), 0);
+            } 
+            else if(map[i][j] == CORNER_TOP_LEFT) {
+
+            } 
+            else if(map[i][j] == CORNER_TOP_RIGHT) {
+
+            } 
+            else if(map[i][j] == CORNER_BOTTOM_LEFT) {
+
+            } 
+            else if(map[i][j] == CORNER_BOTTOM_RIGHT) {
+                al_draw_bitmap(sprite_map.corner_bottom_right, xOff + tile_size * (count % 10), yOff + tile_size * (count / 10), 0);
+            } 
+            else if(map[i][j] == CORNER_LEFT) {
+
+            } 
+            else if(map[i][j] == CORNER_RIGHT) {
+                al_draw_bitmap(sprite_map.corner_right, xOff + tile_size * (count % 10), yOff + tile_size * (count / 10), 0);
+            } 
+            else if(map[i][j] == CORNER_TOP) {
+                
+            } 
+            else if(map[i][j] == CORNER_BOTTOM) {
+                al_draw_bitmap(sprite_map.corner_bottom, xOff + tile_size * (count % 10), yOff + tile_size * (count / 10), 0);
+            } 
+            else if(map[i][j] == FLOOR) {
+                al_draw_bitmap(sprite_map.checkered_floor, xOff + tile_size * (count % 10), yOff + tile_size * (count / 10), 0);
+            } 
+            else if(map[i][j] == WALL) {
+                al_draw_bitmap(sprite_map.wall, xOff + tile_size * (count % 10), yOff + tile_size * (count / 10), 0);
+            }
+
             count++;
         }    
     }
@@ -90,6 +142,11 @@ void map_draw()
 void destroy_sprites_map(void)
 {   
     al_destroy_bitmap(sprite_map.checkered_floor);
+    al_destroy_bitmap(sprite_map.voiding);
+    al_destroy_bitmap(sprite_map.corner_bottom_right);
+    al_destroy_bitmap(sprite_map.corner_left);
+    al_destroy_bitmap(sprite_map.corner_bottom);
+    al_destroy_bitmap(sprite_map.wall);
     return;
 }
 
